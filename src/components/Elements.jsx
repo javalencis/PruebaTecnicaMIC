11b899d8ebfd1048d8111cda30a62fca4fc875da
Jonathan Valencia <javalencis@gmail.com>
2023-03-07 18:48:40 -0500

    indicador del carrito agregado

--- src/components/Elements.jsx
+++ src/components/Elements.jsx
@@ -3,7 +3,7 @@ import { AppContext } from '../context/AppContext'
 import '../styles/Elements.scss'
 
 export const Elements = () => {
-  const { setOpenSideCart } = useContext(AppContext)
+  const {shoppingCart, setOpenSideCart } = useContext(AppContext)
   const handleOpenSideCart = () => {
     setOpenSideCart(e=>!e)
   }
@@ -15,7 +15,12 @@ export const Elements = () => {
         <li><img src="https://moviesshopco.vtexassets.com/assets/vtex/assets-builder/moviesshopco.theme3/3.0.40/user-icon___ef462ae332ac1193a82a58bfde8ac473.svg" alt="" /></li>
         <li><img src="https://moviesshopco.vtexassets.com/assets/vtex/assets-builder/moviesshopco.theme3/3.0.40/heart-icon___5e5273482e1c181e22053a06bc56956d.svg" alt="" /></li>
         <li><img src="https://moviesshopco.vtexassets.com/arquivos/map-locator-2.svg" alt="" /></li>
-        <li onClick={handleOpenSideCart}><img src="https://moviesshopco.vtexassets.com/assets/vtex/assets-builder/moviesshopco.theme3/3.0.40/shopping___eba0ed4ccfbadce9d287241eabeed60f.svg" alt="" /></li>
+        <li onClick={handleOpenSideCart}><img src="https://moviesshopco.vtexassets.com/assets/vtex/assets-builder/moviesshopco.theme3/3.0.40/shopping___eba0ed4ccfbadce9d287241eabeed60f.svg" alt="" />
+        {
+          shoppingCart.length>0 && ( <span className='cartIndicator'>{shoppingCart.length}</span>)
+     
+        }
+        </li>
       </ul>
     </div>
   )
