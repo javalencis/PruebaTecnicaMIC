7cb7f3d4b3fe534ad3fab01b62735df2c1d6cd28
Jonathan Valencia <javalencis@gmail.com>
2023-03-07 19:27:40 -0500

    Scroll slide lateral en tablet y mobile

--- src/components/SlideSmall.jsx
+++ src/components/SlideSmall.jsx
@@ -12,20 +12,20 @@ export const SlideSmall = ({ images, setImgClicked }) => {
 
     const handleTopBottom = (e) => {
         let imgH = myImg.current.clientHeight + 10
-      
+
         if (e.target.id === 'top') {
-            
-                setPosX(-(positions - 2) * imgH) 
-                setPosY(-(positions - 2) * imgH)
-                setPositions(x => x > 0 ? x - 2 : 2)
-         
+
+            setPosX(-(positions - 3) * imgH)
+            setPosY(-(positions - 3) * imgH)
+            setPositions(x => x > 0 ? x - 3 : 3)
+
 
         } else {
-           
-                setPosX(-(positions + 2) * imgH)
-                setPosY(-(positions + 2) * imgH)
-                setPositions(x => x < images.length ? x + 2 : x)
-         
+
+            setPosX(-(positions + 3) * imgH)
+            setPosY(-(positions + 3) * imgH)
+            setPositions(x => x < images.length ? x + 3 : x)
+
 
         }
 
@@ -46,16 +46,18 @@ export const SlideSmall = ({ images, setImgClicked }) => {
                     {'<'}
                 </span>
             </button>
-            <div
-                className='SlideSmallContainer'
-                style={{
-                    transform: window.innerWidth <= 1300 ?'translateX(' + posX+ 'px)':'translateY(' + posY + 'px)'
-                }}>
-                {
-                    images.map((img, index) => (
-                        <img ref={myImg} key={index} id={index} src={img} onClick={handleClickImg}/>
-                    ))
-                }
+            <div className='ScrollContainer'>
+                <div
+                    className='SlideSmallContainer'
+                    style={{
+                        transform: window.innerWidth <= 1300 ? 'translateX(' + posX + 'px)' : 'translateY(' + posY + 'px)'
+                    }}>
+                    {
+                        images.map((img, index) => (
+                            <img ref={myImg} key={index} id={index} src={img} onClick={handleClickImg} />
+                        ))
+                    }
+                </div>
             </div>
             <button
                 id="bottom"
